c0036bd30c72121ffa113982cb7a9634e904ddf0
Francesco Pandolfi <francesco.pan@gmail.com>
2020-03-30 17:19:16 +0200

    Admin show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,8 +4,7 @@
 
     <div class="container">
         <div class="row">
-                @foreach ($posts as $post)
-                    <div class="col-lg-4 col-md-6 col-xs-12"> 
+                    <div class="col"> 
                         <div class="card mb-3">
                             <div class="card-body">
                                 <h2 class="card-title">{{ $post->title }}</h2>
@@ -14,15 +13,13 @@
                                 <p class="card-text">Updated : {{ $post->updated_at }}</p>
                                 <p class="card-text">Created : {{ $post->created_at }}</p>
 
-                                {{-- BUTTONS --}}
+                                {{-- SHOW BUTTON --}}
                                 <a href="{{route('admin.posts.destroy', $post->slug)}}" class="btn btn-danger float-right mr-3">Delete</a>
                                 <a href="{{route('admin.posts.edit', $post->slug)}}" class="btn btn-success float-right mr-3">Edit</a>
-                                <a href="{{route('admin.posts.show', $post->slug)}}" class="btn btn-success float-right mr-3">Read</a>
                                 
                             </div>
                         </div>
                     </div>
-                @endforeach
         </div>
     </div>  
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Auth::routes();
 Route::get('/posts', 'PostController@index')->name('posts.index');
 
 
-Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function()
+Route::prefix('admin')->namespace('Admin')->name('admin.')->middleware('auth')->group(function()
 {
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('posts', 'PostController');
